cf7f4525630935fc51fbf5ab178583f54556e399
Mathias Thorén <mathias.thoren@consid.se>
2026-01-05 18:03:50 +0100

    simplify package exports

--- packages/js/vite.config.ts
+++ packages/js/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -7,30 +6,23 @@ export default defineConfig({
 		// Library entry points
 		lib: {
 			entry: {
-				index: resolve(__dirname, 'src/index.ts'),
+				index: resolve(__dirname, 'src/tarragon.ts'),
 				// Add more entries here for other components you want to expose directly
 				// e.g., button: resolve(__dirname, 'src/button.ts'),
 			},
+			name: 'tarragon',
 			formats: ['es'],
+			fileName: () => 'tarragon.js',
 		},
 		rollupOptions: {
 			// Externalize deps that shouldn't be bundled with your library
 			external: [], // Add any external dependencies here, e.g., ['react', 'vue']
 			output: {
-				// Control where the output files go
-				// e.g., output/dropdown.js, output/dialog.js etc.
-				entryFileNames: '[name].js', // This keeps entry file names like 'index.js', 'dropdown.js'
+				exports: 'named',
 			},
 		},
 		// Output directory
 		outDir: 'dist',
 		emptyOutDir: true, // Clean the dist directory before building
 	},
-	plugins: [
-		dts({
-			entryRoot: 'src', // Specifies the root directory of your source files
-			outDir: 'dist', // Output directory for d.ts files
-			tsconfigPath: './tsconfig.json', // Path to your tsconfig.json
-		}),
-	],
 })
