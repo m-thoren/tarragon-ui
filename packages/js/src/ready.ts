c74a488fa4d97317b582e6ad623cdc6563a71a41
Mathias Thorén <mathias.thoren@consid.se>
2026-05-13 18:53:42 +0200

    remove debug log in ready

--- packages/js/src/ready.ts
+++ packages/js/src/ready.ts
@@ -1,6 +1,5 @@
 import { tuiAttribute } from './constants'
 
 export function ready(component: HTMLElement) {
-	console.debug('🚀 ~ ready ~ component:', component)
 	component.setAttribute(tuiAttribute('state'), 'ready')
 }
