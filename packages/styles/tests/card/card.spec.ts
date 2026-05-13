3f16718bc57d1bb46bda5906ad9ba8fc6e7d651b
Mathias Thorén <mathias.thoren@consid.se>
2026-05-13 18:43:19 +0200

    add snapshot tests for all components

---
+++ packages/styles/tests/card/card.spec.ts
@@ -0,0 +1,16 @@
+import test, { expect } from '@playwright/test'
+
+const componentName = 'card'
+const testPath = '/tests/card'
+
+test.describe(componentName, () => {
+	test('should match screenshot - dark', async ({ page }) => {
+		await page.goto(`${testPath}/dark.html`)
+		await expect(page).toHaveScreenshot()
+	})
+
+	test('should match screenshot - light', async ({ page }) => {
+		await page.goto(`${testPath}/light.html`)
+		await expect(page).toHaveScreenshot()
+	})
+})
