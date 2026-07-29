7398b3861951e65a78ff3363fb435f27dc241ae2
Mathias Thorén <mathiaslthoren@gmail.com>
2026-07-29 09:22:45 +0200

    add typescript declaration file to styles package

---
+++ packages/styles/src/tarragon-ui.d.ts
@@ -0,0 +1,9 @@
+declare module '@tarragon-ui/styles' {
+	const content: unknown
+	export default content
+}
+
+declare module '@tarragon-ui/styles/dev' {
+	const content: unknown
+	export default content
+}
