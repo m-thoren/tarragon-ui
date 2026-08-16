953732e5c3e36149d07f7eecd23e9ca6af144d1e
Mathias Thorén <mathiaslthoren@gmail.com>
2026-08-16 11:00:14 +0200

    fix: add card-image class to card snapshot test

--- packages/styles/src/components/card/tests/card.spec.ts
+++ packages/styles/src/components/card/tests/card.spec.ts
@@ -4,17 +4,19 @@ const componentName = 'card'
 const testPath = `src/components/${componentName}/tests`
 
 test.describe(componentName, () => {
-test.beforeEach(async ({ page }) => {
-await page.goto(`${testPath}/default.html`)
-})
+	test.beforeEach(async ({ page }) => {
+		await page.goto(`${testPath}/default.html`)
+	})
 
-test('should match screenshot - dark', async ({ page }) => {
-await expect(page).toHaveScreenshot()
-})
+	test('should match screenshot - dark', async ({ page }) => {
+		await expect(page).toHaveScreenshot()
+	})
 
-test('should match screenshot - light', async ({ page }) => {
-const body = page.locator('body')
-await body.evaluate((el) => { el.classList.add('light') })
-await expect(page).toHaveScreenshot()
-})
+	test('should match screenshot - light', async ({ page }) => {
+		const body = page.locator('body')
+		await body.evaluate((el) => {
+			el.classList.add('light')
+		})
+		await expect(page).toHaveScreenshot()
+	})
 })
